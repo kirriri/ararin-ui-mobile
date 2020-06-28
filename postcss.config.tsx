ba7bf3e15d9244a277d3f33c0e8d93572e4a58d0
kirriri <ararinn@outlook.com>
2020-06-28 22:37:26 +0800

    dialog eject

---
+++ postcss.config.tsx
@@ -0,0 +1,23 @@
+module.exports = {
+    plugins: {
+      // ...
+      'postcss-px-to-viewport': {
+          unitToConvert: 'px',
+          viewportWidth: 375,
+          viewportHeight: 1334,
+          unitPrecision: 5,
+          propList: ['*'],
+          viewportUnit: 'vw',
+          fontViewportUnit: 'vw',
+          selectorBlackList: [],
+          minPixelValue: 1,
+          mediaQuery: false,
+          replace: true,
+          exclude: undefined,
+          include: undefined,
+          landscape: false,
+          landscapeUnit: 'vw',
+          landscapeWidth: 568
+      }
+    }
+  }
