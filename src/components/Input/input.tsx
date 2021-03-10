25976ec835de0516241be8e55748fd351cd88d2c
kirriri <1229349850@qq.com>
2021-03-11 01:27:21 +0800

    2021-03-11 home_pc dev

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,5 +1,8 @@
 import React, {
-    FC, InputHTMLAttributes
+    FC, 
+    InputHTMLAttributes,
+    useState,
+    useImperativeHandle,
 } from 'react'
 import classNames from 'classnames'
 
@@ -7,7 +10,7 @@ import classNames from 'classnames'
  * Input可适应多种类型校验
  */
 
- type InputType = 'inpit' | 'block'
+ type InputType = 'input' | 'block'
 
  type InputJudge = 'default'| 'mobilePhone'| 'adsl'| 'telPhone'| 'pwd'| 'allIdCard'| 'name'| 'code'| 'idCard'
 
@@ -15,31 +18,64 @@ import classNames from 'classnames'
     title?: string,
     className?: string,
     type?: InputType,
-    judge?: InputJudge
+    judge?: InputJudge,
+    times?: number,
+    onclick?: () => void,
+    codeTxt?: string
  }
 
  type InputProps = BaseInputProps & Omit<InputHTMLAttributes<HTMLElement>, 'type'>
 
  export const Input: FC<InputProps> = props => {
 
-     const {
-         title,
-         className,
-         type,
-         judge
-     } = props
+    const {
+        title,
+        className,
+        type,
+        judge,
+        onclick,
+        codeTxt,
+        times
+    } = props
+
+    const [value, setValue] = useState<string | number>('')
 
     const classes = classNames('ararin-input', className, {
-		[`ararin-button-${type}`]: type,
+		[`ararin-input-${type}`]: type,
     })
-    
 
 
     return  <div className={classes}>
-                <label></label>
-                <input></input>
-                <span></span>
+                { title && <label>{title}</label> }
+                { type === 'input' &&
+                    <React.Fragment>
+                        <input 
+                            value={value}
+                            onChange={e => { e.persist(); setValue(() => { return e.target.value })}}
+                        />
+                        <span></span>
+                    </React.Fragment>
+                }
+                { judge === 'code' &&
+                    ( times > 0 ?
+                        <span className="ararin-disabled">
+                            {times}s
+                        </span> : 
+                        <span 
+                            onClick={onclick}
+                        >
+                            {codeTxt}
+                        </span>
+                    )
+                }
             </div>
  }
+
+ Input.defaultProps = {
+    type: 'input',
+    judge: 'default',
+    title: '',
+    placeholder: ''
+}
 
  export default Input
