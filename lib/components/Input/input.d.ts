a6672d6a6bda8bbba3a2a7494854c389dc23b498
kirriri <1229349850@qq.com>
2021-03-16 10:47:16 +0800

    2021-03-16 mobile_pc dev

--- lib/components/Input/input.d.ts
+++ lib/components/Input/input.d.ts
@@ -1,1 +1,18 @@
-export {};
+import { FC, InputHTMLAttributes } from 'react';
+/**
+ * Input可适应多种类型校验
+ */
+declare type InputType = 'input' | 'block';
+declare type InputJudge = 'default' | 'mobilePhone' | 'adsl' | 'telPhone' | 'pwd' | 'allIdCard' | 'name' | 'code' | 'idCard';
+interface BaseInputProps {
+    title?: string;
+    className?: string;
+    type?: InputType;
+    judge?: InputJudge;
+    times?: number;
+    onclick?: () => void;
+    codeTxt?: string;
+}
+declare type InputProps = BaseInputProps & Omit<InputHTMLAttributes<HTMLElement>, 'type'>;
+export declare const Input: FC<InputProps>;
+export default Input;
